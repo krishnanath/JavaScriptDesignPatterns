d99b000edd58de48e0111769fd31a505e31bdb0f
krishnanath <isabella0484@gmail.com>
2018-07-18 15:49:31 +0530

    js

--- 1ProblemWithGlobalScope/js/script.js
+++ 1ProblemWithGlobalScope/js/script.js
@@ -1,13 +1,33 @@
-var leadself = 'Me: ',
-		leadcomputer = "PC: ",
-		aSaid= ["This is a Cyber Chat"],
-		msgYes = "Yes, that's a great idea.",
-		msgNo = "No, that must be a mistake.",
-		aSassyStuff = ["Like mold on books, grow myths on history.",
-									"She moved like a poem and smiled like a sphinx.",
-									"As long as we don’t die, this is gonna be one hell of a story.",
-									"She laughed, and the desert sang.",
-									"You’ve got about as much charm as a dead slug."];
+var o ={
+	leadself :'Me: ',
+			leadcomputer : "PC: ",
+			aSaid : ["This is a Cyber Chat"],
+			msgYes : "Yes, that's a great idea.",
+			msgNo : "No, that must be a mistake.",
+			aSassyStuff : ["Like mold on books, grow myths on history.",
+										"She moved like a poem and smiled like a sphinx.",
+										"As long as we don’t die, this is gonna be one hell of a story.",
+										"She laughed, and the desert sang.",
+										"You’ve got about as much charm as a dead slug."],
+	talk: function (msg){
+		this.echo(leadself + msg);
+	},
+	replayYesNo: function () {
+		var msg = Math.random()>.5 ? msgYes : msgNo;
+		this.echo(leadcomputer + msg );
+
+	},
+	saySassyStuff: function(){
+		var msg = aSassyStuff[ Math.floor(Math.random()* aSassyStuff.length) ];
+		echo(leadcomputer + msg);
+	},
+	echo: function(msg){
+		this.aSaid.push(<)
+
+	}
+};
+
+   
 
 
 function talk(msg){
